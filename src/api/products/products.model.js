7ff25a94882cbd4f08df529d233a19acb16c83f0
Victor <utrera1695@gmail.com>
2020-06-15 12:40:51 -0400

    test

--- src/api/products/products.model.js
+++ src/api/products/products.model.js
@@ -1,54 +1,63 @@
 'use strict';
 
-import {
-  Schema,
-  model
-} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const ProductModel = Schema({
-  name: {
-    type: String,
-  },
-  description: {
-    type: String
-  },
-  model: {
-    type: String
-  },
-  brand: {
-    type: String
-  },
-  type: {
-    type: String,
-    enum: [1, 2, 3]
-  },
-  /* 
+const ProductModel = Schema(
+  {
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    model: {
+      type: String,
+    },
+    brand: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: [1, 2, 3, 4],
+    },
+    /* 
     1: Monturas
     2: Cristales
     3: Lentes de contactos
+    4: Accesorios
   */
-  price: Number,
-  images: [{
-    type: String
-  }],
-  images_tester: [{
-    type: String
-  }],
-  category: String,
-  stock_status: {
-    type: Boolean,
-    default: true
+    sex: String,
+    young: Boolean,
+    price: Number,
+    offer: Boolean,
+    offerpercent: Number,
+    colors: [{ type: String }],
+    images: [
+      {
+        type: String,
+      },
+    ],
+    images_tester: [
+      {
+        type: String,
+      },
+    ],
+    category: String,
+    stock_status: {
+      type: Boolean,
+      default: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
   },
-  status: {
-    type: Boolean,
-    default: true
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
   }
-
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+);
 
 export default model('Product', ProductModel);
