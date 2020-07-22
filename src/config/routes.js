8d4b2fcb2e8817f54d83c7f2def2ee27a8b3d1d2
Victor <utrera1695@gmail.com>
2020-07-22 13:00:42 -0400

    test

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,12 @@ import UserController from '../api/user/user.controller'
 import CategoryController from '../api/category/category.controller'
 import ProductController from '../api/products/products.controller'
 import OrderController from '../api/order/order.controller'
+import CriticsController from "../api/critics/critics.controller";
 var router = express.Router();
 
 router.use('/api', UserController)
 router.use('/api', CategoryController)
 router.use('/api', ProductController)
 router.use('/api', OrderController)
+router.use('/api', CriticsController)
 export default router;
