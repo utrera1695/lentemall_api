94c1d09e2d929bf26424c7d260377890afc88d0d
Victor <utrera1695@gmail.com>
2020-07-24 08:58:54 -0400

    asd

--- src/api/order/order.model.js
+++ src/api/order/order.model.js
@@ -1,43 +1,47 @@
 'use strict';
 
-import {
-  Schema,
-  model
-} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const OrderModel = Schema({
-  status: {
-    type: String,
-    default: '1'
+const OrderModel = Schema(
+  {
+    status: {
+      type: String,
+      default: '1',
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+    od: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    oi: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    add: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    dp: String,
+    alt: String,
+    doctor: String,
+    notes: String,
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: 'Product'
-  },
-  od: {
-    esf: String,
-    cil: String,
-    eje: String
-  },
-  oi: {
-    esf: String,
-    cil: String,
-    eje: String
-  },
-  add: String,
-  dp: String,
-  alt: String,
-  notes: String
-
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
   }
-});
+);
 
 export default model('Order', OrderModel);
