3eda5d70c58a5587880d96e4ec5b8c55e74758a9
Victor <utrera1695@gmail.com>
2020-08-08 23:57:46 -0400

    test

--- src/api/order/order.model.js
+++ src/api/order/order.model.js
@@ -1,48 +1,53 @@
 'use strict';
 
-import { Schema, model } from 'mongoose';
+import {
+  Schema,
+  model
+} from 'mongoose';
 import autoIncrement from 'mongoose-auto-increment';
-const OrderModel = Schema(
-  {
-    status: {
-      type: String,
-      default: '1',
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-    },
-    product: {
-      type: Schema.Types.ObjectId,
-      ref: 'Product',
-    },
-    od: {
-      esf: String,
-      cil: String,
-      eje: String,
-    },
-    oi: {
-      esf: String,
-      cil: String,
-      eje: String,
-    },
-    add: {
-      esf: String,
-      cil: String,
-      eje: String,
-    },
-    dp: String,
-    alt: String,
-    doctor: String,
-    notes: String,
+const OrderModel = Schema({
+  status: {
+    type: String,
+    default: '1',
   },
-  {
-    timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  od: {
+    esf: String,
+    cil: String,
+    eje: String,
+  },
+  oi: {
+    esf: String,
+    cil: String,
+    eje: String,
+  },
+  add: {
+    esf: String,
+    cil: String,
+    eje: String,
+  },
+  dp: String,
+  alt: String,
+  doctor: String,
+  notes: String,
+  orderNumber: {
+    type: Number,
+    default: 0,
+    unique: true
   }
-);
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
 OrderModel.plugin(autoIncrement.plugin, {
   model: 'Order',
   field: 'orderNumber',
