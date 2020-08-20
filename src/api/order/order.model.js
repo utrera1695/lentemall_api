dfb942c76ff6f7ef7332b16d45827244ba92cddb
Victor <utrera1695@gmail.com>
2020-08-20 14:21:16 -0400

    test

--- src/api/order/order.model.js
+++ src/api/order/order.model.js
@@ -1,67 +1,60 @@
 'use strict';
 
-import {
-  Schema,
-  model
-} from 'mongoose';
-import {
-  autoIncrement
-} from 'mongoose-plugin-autoinc';
+import { Schema, model } from 'mongoose';
+import { autoIncrement } from 'mongoose-plugin-autoinc';
 
-const OrderModel = Schema({
-  status: {
-    type: String,
-    default: '1',
+const OrderModel = Schema(
+  {
+    status: {
+      type: String,
+      default: '1',
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+    od: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    oi: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    add: {
+      esf: String,
+      cil: String,
+      eje: String,
+    },
+    dp: String,
+    alt: String,
+    doctor: String,
+    notes: String,
+    color: String,
+    size: {
+      bridge: Number,
+      horizontal: Number,
+      vertical: Number,
+      diagonal: Number,
+      width: Number,
+      dipstick: Number,
+    },
+    recipe: String,
+    files: [String],
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: 'Product',
-  },
-  od: {
-    esf: String,
-    cil: String,
-    eje: String,
-  },
-  oi: {
-    esf: String,
-    cil: String,
-    eje: String,
-  },
-  add: {
-    esf: String,
-    cil: String,
-    eje: String,
-  },
-  dp: String,
-  alt: String,
-  doctor: String,
-  notes: String,
-  orderNumber: {
-    type: Number,
-    default: 1,
-    unique: true,
-  },
-  color: String,
-  size: {
-    bridge: Number,
-    horizontal: Number,
-    vertical: Number,
-    diagonal: Number,
-    width: Number,
-    dipstick: Number,
-  },
-  recipe: String,
-  files: [String],
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  },
-});
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  }
+);
 OrderModel.plugin(autoIncrement, {
   model: 'Order',
   field: 'orderNumber',
