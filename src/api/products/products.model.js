372a21cffb5572fbd93ba77bdbe121462cafd399
Victor <utrera1695@gmail.com>
2020-08-28 17:39:49 -0400

    test

--- src/api/products/products.model.js
+++ src/api/products/products.model.js
@@ -1,75 +1,83 @@
 'use strict';
 
-import {
-  Schema,
-  model
-} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const ProductModel = Schema({
-  name: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  model: {
-    type: String,
-  },
-  brand: {
-    type: String,
-  },
-  type: {
-    type: String,
-    enum: [1, 2, 3, 4],
-  },
-  /* 
+const ProductModel = Schema(
+  {
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    model: {
+      type: String,
+    },
+    brand: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: [1, 2, 3, 4],
+    },
+    /* 
     1: Monturas
     2: Cristales
     3: Lentes de contactos
     4: Accesorios
   */
-  sex: String,
-  young: Boolean,
-  price: Number,
-  offer: Boolean,
-  offerpercent: Number,
-  colors: [{
-    type: String
-  }],
-  images: [{
-    type: String,
-  }, ],
-  images_tester: [{
-    type: String,
-  }, ],
-  category: String,
-  stock_status: {
-    type: Boolean,
-    default: true,
+    sex: String,
+    young: Boolean,
+    price: Number,
+    offer: Boolean,
+    offerpercent: Number,
+    colors: [
+      {
+        type: String,
+      },
+    ],
+    images: [
+      {
+        type: String,
+      },
+    ],
+    images_tester: [
+      {
+        type: String,
+      },
+    ],
+    category: String,
+    stock_status: {
+      type: Boolean,
+      default: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    sizes: [
+      {
+        bridge: Number,
+        horizontal: Number,
+        vertical: Number,
+        diagonal: Number,
+        width: Number,
+        dipstick: Number,
+      },
+    ],
+    material: { type: String, default: '' },
+    shape: { type: String, default: '' },
+    outstanding: {
+      type: Boolean,
+      default: false,
+    },
   },
-  status: {
-    type: Boolean,
-    default: true,
-  },
-  sizes: [{
-    bridge: Number,
-    horizontal: Number,
-    vertical: Number,
-    diagonal: Number,
-    width: Number,
-    dipstick: Number
-  }],
-  material: String,
-  shape: String,
-  outstanding: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
   }
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  },
-});
+);
 
 export default model('Product', ProductModel);
